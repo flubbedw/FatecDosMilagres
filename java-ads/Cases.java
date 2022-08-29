5317b41a6d411197ea7c835400a6c6c2017d0655
Julio Vanderlei <80010937+flubbedw@users.noreply.github.com>
2022-08-29 10:54:37 -0300

    Add files via upload

---
+++ java-ads/Cases.java
@@ -0,0 +1,62 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main{
+    static Scanner scanner;
+    
+    
+    static void geraVetor(int[] v, int limite, int acrescimo){
+        Random random = new Random();
+        for (int i=0; i<v.length; i++)
+           v[i] = random.nextInt(limite) + acrescimo;
+}
+    static void exibeVetor(int[] v, String msg){
+        System.out.println("\n" + msg);
+        for(int i=0; i<v.length; i++)
+           System.out.print(v[i] + " ");
+        System.out.println();
+    
+}
+    static int menu() {
+        System.out.println ("Escolha a sua acao:");
+        System.out.println (" 1 - Jogar na Megasena");
+        System.out.println (" 2 - Jogar dados");
+        System.out.println (" 3 - Sair");
+       return scanner.nextInt();
+    }
+    
+    
+     public static void main(String[] args){
+        scanner = new Scanner(System.in);
+        int opcao;
+     do{
+        opcao = menu();
+        System.out.println();
+        
+        switch (opcao){
+            case 1:
+               int sena[] = new int[6];
+                geraVetor (sena, 60, 1);
+                exibeVetor(sena, "Vamos ficar ricos!!!!!");
+                break;
+        
+            case 2:
+                System.out.println("Digite a quantidade de lancamentos: ");
+                int numLancamentos = scanner.nextInt();
+                int[] lancamentos = new int[numLancamentos];//instancia
+                geraVetor(lancamentos, 6, 1);
+                exibeVetor(lancamentos, "Rolam os dados");
+                break;
+            case 3:
+                System.out.println("Obrigado(a), volte sempre!");
+                break;
+            
+            default:
+                System.out.println("Opcao invalida!");
+        }
+     }while (opcao !=3);
+        
+        scanner.close();
+        
+        }
+} 
---
+++ java-ads/Dados.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main{
+
+    static void geraVetor (int[] v, int limite, int acrescimo) {
+        Random random = new Random();
+        for (int i=0; i<v.length; i++) 
+            v[i] = random.nextInt(limite) + acrescimo;
+    }
+    static void exibeVetor (int[] v, String msg) {
+        System.out.println ("\n" + msg + "\n");
+        for (int i=0; i<v.length; i++) 
+            System.out.print (v[i] + " ");
+        System.out.println();
+    }
+ 
+    public static void main(String[] args){
+        int sena[] = new int[6];
+        geraVetor (sena, 60, 1);
+        exibeVetor(sena, "Vamos ficar ricos!");
+        
+        
+        int[] lancamentos;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println ("digite a quantidade de lancamentos: ");
+        int numLancamentos = scanner.nextInt();
+        lancamentos  = new int[numLancamentos];
+        geraVetor(lancamentos, 6, 1);
+        exibeVetor (lancamentos, "Rolam os dados");
+    } 
+}
