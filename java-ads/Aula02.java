45e32ecc9b7f2b80ed1e561841650d656e86db23
Julio Vanderlei <80010937+flubbedw@users.noreply.github.com>
2022-09-05 14:07:56 +0000

    modified:   java-ads/Aula02.java

--- java-ads/Aula02.java
+++ java-ads/Aula02.java
@@ -1,5 +1,25 @@
 public class Aula02 {
-    public static void main(String[] args){
-        
+    private static final int i = 0;
+    public static void parametroValor (int n) {
+        n = n + 10;
+        System.out.println ("n dentro do metodo: " + n);
+    }
+    static void parametroreferencia (int v[]) {
+        for (int i=0; i<v.length; i++);
+            v[i] = 10;
+    }
+    public static void main (String args[]) {
+        int n=2;
+        System.out.println("n antes do metodo: " + n);
+        parametroValor(n);
+        System.out.println("n depois do metodo: " + n);
+        int v[] = new int[5];
+        for (int i=0; i<5; i++) {
+            v[1] = 1;
+        }
+        parametroreferencia(v);
+        System.out.println("Veja o que aconteceu com o vetor:");
+        for (int i=0; i<5; i++);
+            System.out.println (v[i] + " ");
     }
 }
