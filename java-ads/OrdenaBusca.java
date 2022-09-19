f7611b81fcc663e667bdbcf260569fb1d38c8875
Julio Vanderlei <80010937+flubbedw@users.noreply.github.com>
2022-09-19 10:47:34 -0300

    Update OrdenaBusca.java

--- java-ads/OrdenaBusca.java
+++ java-ads/OrdenaBusca.java
@@ -1,47 +1,76 @@
-import java.util.Scanner;
 import java.util.Random;
-import java.util.Date;
+import java.util.Scanner;
 
 public class OrdenaBusca {
-	static Scanner scanner;
-	static Random random;
+    static Scanner scanner;
+    static Random random;
 
-	public static void main(String args[]) {
-
-	static void geraVetor (int[] v, int limite, int acrescimo) {
-        Random random = new Random();
-        for (int i = 0 ; i < v.length; i++) {
-            v[i] = random.nextInt(limite) + acrescimo;
-   	 }
-	}
-
-	static void exibeVetor (int[] v, String msg) {
-        System.out.println ("\n" + msg);
-        for (int i = 0; i < v.length; i++) {
-            System.out.print (v[i] + " ");
+    static void geraVetor (int v[]) {
+        for (int i=0; i<v.length; i++)
+            v[i] = random.nextInt(10 * v.length);
+    }
+    static void exibeVetor (int v[], String msg) {
+        System.out.println("\n" + msg);
+        for (int i=0; i<v.length; i++) 
+            System.out.print(v[i] + " ");
+        System.out.println();
+    }
+    static void insertion (int v[]) {
+        for(int i=1; i<v.length; i++) {
+            int x = v[i];
+            int j = i-1;
+            for ( ; j>=0 && v[j]>x; j--) {
+                v[j+1] = v[j];
+            }
+            v[j+1] = x;
         }
-        System.out.println("\n");
-	}
-
-        static void insertion(int v[]){
-            for(int i = 1; i<v.length; i++){
-                int x = v[i];
-                int j = i-1;
-                for(int j = 1 - 1; j>=0&& v[j] > x; j --){
-                    v[j+1]= v[j];
-                }
-                v[j+1] = x;
+    }
+    static boolean buscaSimples (int x, int[] v) {
+        int cont=0;
+        boolean achou=false;
+        for (int i=0; i<v.length; i++){
+            cont++;
+            if (v[i] == x) achou = true;
+        }
+        System.out.println("busca simples realizou " + cont + " comparacoes");
+        return achou;
+    }
+    static boolean buscaBinaria (int x, int[] v) {
+        int ini=0, fim=v.length-1;
+        int cont=0;
+        boolean achou=false;
+        while (ini <= fim) {
+            cont++;
+            int meio = (ini + fim)/2;
+            if (x == v[meio]){
+                achou = true;
+            }
+            else {
+                cont++;
+                if (x > v[meio]) ini = meio + 1;
+                else fim = meio - 1;
             }
         }
-        
-	    static boolean buscaSimples(int x,int[]v){
-	        for(int i= 0; i<v.length; i++)
-	            if(v[i] == x)
-	            return true;
-	        return false;
-	    }
-	    static boolean buscaBinaria(int x, int [] v){
-	        int ini= 0, fim = v.length-1;
-	    }
-	}
+        return achou;
+    }
+    public static void main(String[] args) {
+        random = new Random();
+        scanner = new Scanner(System.in);
+        int n;
+        do {
+            System.out.print("digite o tamanho do vetor: ");
+            n = scanner.nextInt();
+            if (n>0) {
+                int v[] = new int[n];
+                geraVetor(v);
+                exibeVetor(v, "vetor gerado");
+                System.out.print("digite o valor para busca: ");
+                int x = scanner.nextInt();
+                if (buscaSimples(x, v))
+                    System.out.println("elemento encontrado na busca simples");
+                else 
+                    System.out.println("elemento nao encontrado na busca simples");
+            }
+        } while (n>0);
+    }
 }
